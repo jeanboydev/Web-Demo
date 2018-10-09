c367706f941238a18614e36e45f605da982a4d21
jeanboy <jeanboy@foxmail.com>
2018-10-09 20:32:12 +0800

    update

---
+++ src/main/webapp/WEB-INF/static/js/console_auth.js
@@ -0,0 +1,34 @@
+function onCreate(tab) {
+    $('#dialogCreate').modal();
+}
+
+function onCreateSave(tab, token) {
+    var roleName = $('#role_name').val();
+    console.error("===tab==" + tab);
+    console.error("===roleName==" + roleName);
+    if (roleName === ""){
+        showToast("角色名", "不能为空！");
+        showToast("角色名", "不能为空！");
+        return;
+    }
+    $.ajax(host + "/role", {
+        headers: {
+            token: encodeURIComponent(token)
+        },
+        method: "put",
+        dataType: "json",
+        data: {
+            name: roleName
+        },
+        success: function (data) {
+            window.location.reload();
+        },
+        error: function (error) {
+            console.error(error.responseText);
+            if (error.responseJSON) {
+                var info = error.responseJSON.errors[0];
+                alert(info.description);
+            }
+        }
+    });
+}
